0fafef579c998130b710b9d04186580cce3e6237
David Reyes <david.reyes182006@gmail.com>
2025-11-20 19:39:27 -0300

    mejorando el inicio de sesion

--- login.py
+++ login.py
@@ -1,11 +1,42 @@
 import tkinter as tk
+from utils import x, y, MIN_ANCHO, MIN_ALTO
 
+#Creacion de la ventana de Inicio de secion
 class Login():
     def __init__(self):
+        #Configuracion de la ventana
         self.root = tk.Tk()
         self.root.title("Iniciar Seción")
         self.root.config(background="#2b2b2b")
-        self.root.geometry()
-    
+        self.root.geometry(f"{MIN_ANCHO}x{MIN_ALTO}+{x}+{y}")
+        self.root.minsize(MIN_ANCHO, MIN_ALTO)
+        
+        
+        #               Iniciar secion
+        
+        #Label inicio de secion
+        label_inicio = tk.Label(self.root, text="Iniciar sesión", font=('Arial', 30), background="#2b2b2b", fg="#ffffff")
+        label_inicio.place(relx=0.1, rely=0.1)
+        
+        #    Seccion de email
+        
+        label_email = tk.Label(self.root, text="Ingrese su email", font=('Arial', 20), background="#2b2b2b", fg="#ffffff")
+        label_email.place(relx=0.1, rely=0.2)
+        
+        entrada_email = tk.Entry(self.root)
+        entrada_email.place(relx=0.1, rely=0.3)
+        
+        #    Seccion contrasena
+        label_contrasena = tk.Label(self.root, text="Ingrese su contraseña", font=('Arial', 20), background="#2b2b2b", fg="#ffffff")
+        label_contrasena.place(relx=0.1, rely=0.4)
+        
+        entrada_contrasena = tk.Entry(self.root)
+        entrada_contrasena.place(relx=0.1, rely=0.5)
+        
+        #Boton iniciar sesion
+        boton_sesion = tk.Button(self.root, text="Iniciar sesion")
+        boton_sesion.place(relx = 0.1, rely=0.6)
+        
+        
     def iniciar(self):
         self.root.mainloop()
---
+++ utils.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+#Visualizar el alto y ancho de la ventana del computador
+ventana = tk.Tk()
+ventana.withdraw()
+
+ANCHO = ventana.winfo_screenwidth()
+ALTO = ventana.winfo_screenheight()
+
+ventana.destroy()
+
+#El minim,o alto y ancho que debe tener la ventana
+MIN_ANCHO = int(ANCHO * 0.7)
+MIN_ALTO = int(ALTO * 0.7)
+
+
+#Coordenadas para centrar las ventanas
+x = int((ANCHO // 2) - (MIN_ANCHO // 2))
+y = int((ALTO // 2) - (MIN_ALTO // 2))
